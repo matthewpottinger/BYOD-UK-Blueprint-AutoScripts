1105d99e860e6ab7b852f25bdec63d4184ae299c
matthewpottinger <>
2020-07-16 10:06:07 +0100

    Added Export of Assignments

--- DER-Export.ps1
+++ DER-Export.ps1
@@ -166,7 +166,7 @@ NAME: Get-DeviceEnrollmentConfigurations
     [cmdletbinding()]
     
     $graphApiVersion = "Beta"
-    $Resource = "deviceManagement/deviceEnrollmentConfigurations"
+    $Resource = "deviceManagement/deviceEnrollmentConfigurations?`$expand=assignments"
         
         try {
             
@@ -193,6 +193,188 @@ NAME: Get-DeviceEnrollmentConfigurations
     }
 
 ####################################################
+Function Get-AADGroup(){
+
+
+
+	<#
+	
+	.SYNOPSIS
+	
+	This function is used to get AAD Groups from the Graph API REST interface
+	
+	.DESCRIPTION
+	
+	The function connects to the Graph API Interface and gets any Groups registered with AAD
+	
+	.EXAMPLE
+	
+	Get-AADGroup
+	
+	Returns all users registered with Azure AD
+	
+	.NOTES
+	
+	NAME: Get-AADGroup
+	
+	#>
+	
+	
+	
+	[cmdletbinding()]
+	
+	
+	
+	param
+	
+	(
+	
+		$GroupName,
+	
+		$id,
+	
+		[switch]$Members
+	
+	)
+	
+	
+	
+	# Defining Variables
+	
+	$graphApiVersion = "v1.0"
+	
+	$Group_resource = "groups"
+	
+		
+	
+		try {
+	
+	
+	
+			if($id){
+	
+	
+	
+			$uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=id eq '$id'"
+	
+			(Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+	
+	
+	
+			}
+	
+			
+	
+			elseif($GroupName -eq "" -or $GroupName -eq $null){
+	
+			
+	
+			$uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)"
+	
+			(Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+	
+			
+	
+			}
+	
+	
+	
+			else {
+	
+				
+	
+				if(!$Members){
+	
+	
+	
+				$uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
+	
+				(Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+	
+				
+	
+				}
+	
+				
+	
+				elseif($Members){
+	
+				
+	
+				$uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)?`$filter=displayname eq '$GroupName'"
+	
+				$Group = (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+	
+				
+	
+					if($Group){
+	
+	
+	
+					$GID = $Group.id
+	
+	
+	
+					$Group.displayName
+	
+					write-host
+	
+	
+	
+					$uri = "https://graph.microsoft.com/$graphApiVersion/$($Group_resource)/$GID/Members"
+	
+					(Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+	
+	
+	
+					}
+	
+	
+	
+				}
+	
+			
+	
+			}
+	
+	
+	
+		}
+	
+	
+	
+		catch {
+	
+	
+	
+		$ex = $_.Exception
+	
+		$errorResponse = $ex.Response.GetResponseStream()
+	
+		$reader = New-Object System.IO.StreamReader($errorResponse)
+	
+		$reader.BaseStream.Position = 0
+	
+		$reader.DiscardBufferedData()
+	
+		$responseBody = $reader.ReadToEnd();
+	
+		Write-Host "Response content:`n$responseBody" -f Red
+	
+		Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+	
+		write-host
+	
+		break
+	
+	
+	
+		}
+	
+	
+	
+	}
+		
 
 #region Authentication
 
@@ -290,8 +472,26 @@ foreach ($DEC in $DeviceEnrollmentConfigurations)
 	
 	$FileName = $($DEC.displayName) -replace '\<|\>|:|"|/|\\|\||\?|\*', "_"
 	New-Item "$ExportPath\$($FileName).json" -ItemType File -Force
-   
-    $JSON_DATA = $DEC | ConvertTo-Json -depth 5
+
+    $DEC.assignments.target | 
+
+    ForEach-Object {
+     
+       $GroupID = $_.GroupId
+
+       write-host "GroupID" $GroupID
+
+       $AADGroup = (Get-AADGroup -id $GroupId)
+
+       
+       write-host "GroupName" $AADGroup.DisplayName
+       $_.GroupID = $AADGroup.DisplayName
+    
+    }
+
+    $DEC.assignments.target
+    
+    $JSON_DATA = $DEC | ConvertTo-Json -depth 10
 
     $JSON_Data | Out-File -FilePath "$ExportPath\$($FileName).json" -Append -Encoding ascii
 
